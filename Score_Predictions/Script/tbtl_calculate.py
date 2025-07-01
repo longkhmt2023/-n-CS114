cecdf27eb63ec5335045f140426a6673357312a3
meofc <23521787@ms.uit.edu.vn>
2025-07-01 19:22:54 +0700

    Major update

--- Score_Predictions/Script/tbtl_calculate.py
+++ Score_Predictions/Script/tbtl_calculate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-input_df = pd.read_csv('../Input - Output/output.csv')
-input_df['tbtl'] = 0.2*input_df['qt'] + 0.4*input_df['th']+0.4*input_df['ck']
-input_df.to_csv('../Input - Output/output.csv', index=False)
+input_df = pd.read_csv('../Input - Output/Output.csv')
+input_df1 = pd.read_csv('../Input - Output/Output1.csv')
+input_df['tbtl'] = 0.2*input_df1['predicted_qt'] + 0.4*input_df['predicted_th']+0.4*input_df['predicted_ck']
+input_df.to_csv('../Input - Output/Output2.csv', index=False)
